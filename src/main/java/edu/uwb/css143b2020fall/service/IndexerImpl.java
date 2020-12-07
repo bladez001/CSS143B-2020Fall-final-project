de97b6a37b11e9fabd3deb6754558712af460d29
Timothy Muresan <muresan_timothy@yahoo.com>
2020-12-06 19:19:45 -0800

    Finished IndexerImpl

--- src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/IndexerImpl.java
@@ -15,6 +15,24 @@ public class IndexerImpl implements Indexer {
         for (int docsCounter = 0; docsCounter < docs.size(); docsCounter++) {
             String splitDoc = docs.get(docsCounter);
             String[] words = splitDoc.split("\\s+");
+            int wordsCounter = 0;
+
+            for (String word : words) {
+                if (word.equals("")) {
+                    continue;
+                }
+                if (!indexes.containsKey(word)) {
+                    List<List<Integer>> docsList = new ArrayList<>();
+                    for (int j = 0; j < docs.size(); j++) {
+                        docsList.add(new ArrayList<>());
+                    }
+                    indexes.put(word, docsList);
+                }
+                List<List<Integer>> docsList = indexes.get(word);
+                List<Integer> indexList = docsList.get(docsCounter);
+                indexList.add(wordsCounter);
+                wordsCounter++;
+            }
         }
         return indexes;
     }
