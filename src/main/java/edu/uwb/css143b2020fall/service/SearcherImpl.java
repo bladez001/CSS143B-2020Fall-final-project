2fa22b810e38fe903adcf205413ff7a655a2a7ee
Timothy Muresan <muresan_timothy@yahoo.com>
2020-12-09 17:19:04 -0800

    Started SearcherImpl

--- src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
+++ src/main/java/edu/uwb/css143b2020fall/service/SearcherImpl.java
@@ -10,7 +10,45 @@ import java.util.Map;
 public class SearcherImpl implements Searcher {
     public List<Integer> search(String keyPhrase, Map<String, List<List<Integer>>> index) {
         List<Integer> result = new ArrayList<>();
-        // add your code
+
+        String[] wordSplit = keyPhrase.split("\\s+");
+
         return result;
     }
 }
+        //comments from lecture
+        // Step 0: split keyPhrases into words by spaces
+
+        // Step 1: get the documents that contain all the words in the given phrases
+        // [0,1,3,4,5], [0,2,3,4,5]
+        // get the common number (document id) of both lists --> [0,3,4,5]
+
+        // step 2: for each common document, get the location idx of each word in the search phrase.
+        // "is here already"
+        // document 2: {1}, {2}, {3}
+
+        // determine whether search words are in the correct order right next to each other
+        // if yes doc 2 is one of the answers
+
+        // do the location math and return the documents that have common index after the calculation
+
+        //[3,4,1,2] [1,3,2] [2,7,8,3,5] [8,3,10,2]
+
+        // how to get the intersection of all the arrays?
+
+        // [2,3] ??
+
+        // for each of the numbers in [2,7,8,3,5],
+        // see if it exists in the other array
+
+        //[2,3]
+
+        // hashmap
+        // key: numbers
+        // value: counts
+        // 3 -> 4
+        // 4 -> 1
+        // 1 -> 2
+        // 2 -> 4
+
+
